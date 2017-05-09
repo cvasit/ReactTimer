f7b784b663b3aacf83f06c92d216483df2bbfba6
cvasit <cvasit@gmail.com>
2017-05-09 11:29:19 -0500

    Initial commit

---
+++ app/components/Navigation.js
@@ -0,0 +1,31 @@
+var React=require('react');
+var {Link}=require('react-router');
+var {IndexLink}=require('react-router');
+
+var Navigation=()=>{
+        return(
+            <div className="top-bar">
+                <div className="top-bar-left">
+                    <ul className="menu">
+                        <li>
+                            <Link to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Timer</Link>
+                        </li>
+                        <li>
+                            <Link to="/" activeClassName="active" activeStyle={{fontWeight:'bold'}}>Countdown</Link>
+                        </li>
+                    </ul>
+
+                </div>
+                <div className="top-bar-right">
+                    <ul className="menu">
+                        <li className="menu-text" >
+                            Created By <a href="http://cvasit@gmail.com" target="_blank">Vivek</a>
+                        </li>
+
+                    </ul>
+                </div>
+            </div>
+        )
+    };
+
+module.exports=Navigation;
---
+++ server.js
@@ -0,0 +1,25 @@
+var express =require('express');
+
+//create your app
+var app=express();
+//
+// app.use(function (req,res,next) {
+//     if(req.headers['x-forwarded-proto']=='http')
+//         next();
+//     else
+//         res.redirect('http://' + req.hostname + req.url)
+// });
+
+//tell express which folder to server exposing the folder to webserver
+app.use(express.static('public'));
+
+var portnumber=3000;
+
+app.listen(portnumber,function () {
+
+    //var port=process.env.PORT || portnumber;
+
+    var port= portnumber;
+
+    console.log(`Express server is up on port:${port}`);
+});
